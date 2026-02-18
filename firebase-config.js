3c0b6a3b92f6ecdb09f02abbd14f874f01c2e32d
GOTRUSTPAY <gotrustpayinfo@gmail.com>
2026-02-18 18:48:04 +0200

    Create firebase-config.js
    Ajout fichier config firebase

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+    getFirestore, doc, getDoc, collection, addDoc, updateDoc,
+    query, where, getDocs, orderBy, limit, serverTimestamp, onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwzUODVlb3EdiWqZ4t2X-lWg2TXyeyW4Q",
+    authDomain: "stepnova-1426b.firebaseapp.com",
+    projectId: "stepnova-1426b",
+    storageBucket: "stepnova-1426b.appspot.com",
+    messagingSenderId: "835775629094",
+    appId: "1:835775629094:web:8499cbcd43466bb1e9e3e7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
